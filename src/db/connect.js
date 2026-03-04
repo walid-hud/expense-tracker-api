b331cb7da77910e27f4e738c2cf9545ed4433ee1
walid-hud <walidthehud@gmail.com>
2026-03-04 09:53:55 +0000

    add db name to db coonnection

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
-
-
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI, { dbName: process.env.MONGODB_DB_NAME });
         console.log("MongoDB connected");
     } catch (error) {
         console.error("MongoDB connection error:", error);
