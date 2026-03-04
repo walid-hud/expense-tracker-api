f6c14dd919b4b58a6bbc13e916295a54fb817ebc
walid-hud <walidthehud@gmail.com>
2026-03-04 15:31:49 +0000

    add zod validation midlleware to get transactions

--- src/routes/Transactions.js
+++ src/routes/Transactions.js
@@ -1,10 +1,35 @@
 import express from "express";
-import { GetStats } from "../controllers/transactions/getStart_transactions.js";
+import { GetStats } from "../controllers/transactions/get_stats.js";
 import { GetTransactions } from "../controllers/transactions/get_transactions.js";
 import { PostTransaction } from "../controllers/transactions/post_transactions.js";
+import z from "zod";
+import { zodValidate } from "../middleware/zod_validate.js";
 const router = express.Router();
 
-router.get("/", GetTransactions);
+export const GetTransactionsParamsSchema = z
+    .object({
+        page: z.coerce.number().min(1).optional().default(1),
+        limit: z.coerce.number().min(1).max(100).optional().default(10),
+        date: z.coerce.date().optional(),
+        createdFrom: z.coerce.date().optional(),
+        createdTo: z.coerce.date().optional(),
+        category: z.string().optional(),
+        transactionType: z.enum(["income", "expense"]).optional(),
+        /* example request query: 
+        transactions?page=1&limit=10&date=2024-01-01&createdFrom=2024-01-01&createdTo=2024-12-31&category=food&type=expense
+        */
+    })
+    .refine((data) => {
+        if (data.date && (data.createdFrom || data.createdTo)) {
+            return false;
+        }
+        return true;
+    }, 
+    {
+        message: "either specify date or createdFrom/createdTo, not both",
+    });
+
+router.get("/", zodValidate(GetTransactionsParamsSchema), GetTransactions);
 router.post("/", PostTransaction);
 router.get("/stats", GetStats);
 
